11d964d2bafc5c6b17be9432a8078735645e80b9
manoharanthony <manohar.anthony@wohlig.in>
2019-10-17 18:27:16 +0530

    Json and Http

--- app/app.js
+++ app/app.js
@@ -1,5 +1,19 @@
-var myNinjaApp = angular.module('myNinjaApp',[]);
+var myNinjaApp = angular.module('myNinjaApp',['ngRoute']);
 
+   myNinjaApp.config(['$routeProvider', function($routeProvider){
+      $routeProvider
+      .when('/home', {
+         templateUrl: 'views/home.html'
+      })
+
+      .when('/directory', {
+         templateUrl: 'views/directory.html',
+         controller: 'NinjaController'
+      }).otherwise({
+         redirectTo: '/home'
+      })
+
+   }]);
 // myNinjaApp.config(function(){
 
 // });
@@ -10,7 +24,7 @@ var myNinjaApp = angular.module('myNinjaApp',[]);
 
 // myNinjaApp.controller
 
- myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+ myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $http){
 
    $scope.removeNinja = function(ninja){
       var removedNinja = $scope.ninjas.indexOf(ninja);
@@ -32,59 +46,73 @@ var myNinjaApp = angular.module('myNinjaApp',[]);
    // $scope.message = "Hey y'all"
 
   //  $scope.ninjas= ['crystal','ryu','yoshi','shaun'];
-  $scope.ninjas = [
-     {
-        name:"Yoshi",
-        belt:"orange",
-        rate: 50,
-        available: true,
-        thumb: "content/img/yoshi.png"
-     },
-     {
-        name:"Ryu",
-        belt:"yellow",
-        rate: 30,
-        available: true,
-        thumb: "content/img/shaun.png"
-     },
-     {
-        name:"Crystal",
-        belt:"green",
-        rate: 40,
-        available: true,
-        thumb: "content/img/crystal.jpeg"
-     },
-     {
-        name:"Shaun",
-        belt:"black",
-        rate: 90,
-        available: false,
-        thumb: "content/img/shaun.png"
-     },
-     {
-      name:"Cloud",
-      belt:"blue",
-      rate: 80,
-      available: true,
-      thumb: "content/img/shaun.png"
-   },
-   {
-      name:"Cowboy",
-      belt:"green",
-      rate: 70,
-      available: false,
-      thumb: "content/img/crystal.jpeg"
-   },
-   {
-      name:"Flippy",
-      belt:"black",
-      rate: 100,
-      available: true,
-      thumb: "content/img/yoshi.png"
-   },
+//   $scope.ninjas = [
+//      {
+//         name:"Yoshi",
+//         belt:"orange",
+//         rate: 50,
+//         available: true,
+//         thumb: "content/img/yoshi.png"
+//      },
+//      {
+//         name:"Ryu",
+//         belt:"yellow",
+//         rate: 30,
+//         available: true,
+//         thumb: "content/img/shaun.png"
+//      },
+//      {
+//         name:"Crystal",
+//         belt:"green",
+//         rate: 40,
+//         available: true,
+//         thumb: "content/img/crystal.jpeg"
+//      },
+//      {
+//         name:"Shaun",
+//         belt:"black",
+//         rate: 90,
+//         available: false,
+//         thumb: "content/img/shaun.png"
+//      },
+//      {
+//       name:"Cloud",
+//       belt:"blue",
+//       rate: 80,
+//       available: true,
+//       thumb: "content/img/shaun.png"
+//    },
+//    {
+//       name:"Cowboy",
+//       belt:"green",
+//       rate: 70,
+//       available: false,
+//       thumb: "content/img/crystal.jpeg"
+//    },
+//    {
+//       name:"Flippy",
+//       belt:"black",
+//       rate: 100,
+//       available: true,
+//       thumb: "content/img/yoshi.png"
+//    },
+//   ];
 
+//   console.log(angular.toJson($scope.ninjas));
 
-  ];
+// $http({
+//    method: 'get', 
+//    url: 'data/ninjas.json'
+// }).then(function (data) {
+//    $scope.ninjas = data.data;
+// },function (error){
+//    console.log(error, 'can not get data.');
+// });
+
+   $http.get('data/ninjas.json').then(function (data) {
+      $scope.ninjas = data.data;
+   },function (error){
+      console.log(error, 'can not get data.');
+   });
 
 }]);
-    
